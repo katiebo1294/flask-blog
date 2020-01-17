43dfcaff01621480f3f5b35d273abe45224633ae
Katie <katieborley@hotmail.com>
2020-01-17 22:10:00 +0000

    finished tutorial 6

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '9adaa3b46c68320cce53f962cc2d7ac7' #terminal: python3, import secrets, secrets.token_hex(16)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login' #same param as url_for()
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
