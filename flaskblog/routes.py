6f969caffc36c3150aa0a4b981298bf28318d16b
Katie <katieborley@hotmail.com>
2020-01-18 05:26:35 +0000

    finish tutorial 9

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -8,10 +8,11 @@ from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
 
-@app.route('/')
-@app.route('/home')
+@app.route("/")
+@app.route("/home")
 def home():
-	posts = Post.query.all()
+	page = request.args.get('page', 1, type=int)
+	posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
 	return render_template('home.html', posts=posts)
 
 @app.route("/about")
@@ -127,4 +128,13 @@ def delete_post(post_id):
 	db.session.delete(post)
 	db.session.commit()
 	flash('Your post has been deleted!', 'success')
-	return redirect(url_for('home'))		
+	return redirect(url_for('home'))	
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+	page = request.args.get('page', 1, type=int)
+	user = User.query.filter_by(username=username).first_or_404()
+	posts = Post.query.filter_by(author=user)\
+		.order_by(Post.date_posted.desc())\
+		.paginate(page=page, per_page=5)
+	return render_template('user_posts.html', posts=posts, user=user)		
